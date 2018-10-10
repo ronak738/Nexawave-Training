ae297d8e2493c162594f8e50cdb5872eaf1433f3
ronak738 <ronakbansal738@gmail.com>
2018-10-10 09:51:10 +0530

    first commit

---
+++ src/delloitteinheritance/tem.java
@@ -0,0 +1,12 @@
+package delloitteinheritance;
+
+public class tem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=56A;
+		System.out.println(i);
+
+	}
+
+}
